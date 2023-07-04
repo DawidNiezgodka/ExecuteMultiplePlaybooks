69e26043d0011cda4e69bfd599eff955ad5a691b
Dawid Niezgódka <dawid.niezgodka@gmail.com>
2023-07-04 19:17:33 +0200

    adjusted the logic of isOrderIdentical function

--- index.js
+++ index.js
@@ -107,9 +107,11 @@ function isOrderIdentical(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
+  let sortedArr1 = [...arr1].sort();
+  let sortedArr2 = [...arr2].sort();
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+  for (let i = 0; i < sortedArr1.length; i++) {
+    if (sortedArr1[i] !== sortedArr2[i]) {
       return false;
     }
   }
